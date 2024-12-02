7dd667f1022733ee18ba05b91c91d8523d8ecfdd
LUSA <107564399+LooosaZ@users.noreply.github.com>
2024-12-02 22:12:02 +0000

    added css to perfil

--- public_html/resources/perfil/perfil.php
+++ public_html/resources/perfil/perfil.php
@@ -62,7 +62,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Perfil</title>
+    <link rel="stylesheet" href="perfil.css">
 </head>
+
 
 <body>
 <h1>Editar Perfil</h1>
